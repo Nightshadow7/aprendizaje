837729217fc27cacbd61efab2c0b7d0f91dd34ca
Nightshadow7 <hernandezdiazanthonilexandre@gmail.com>
2023-09-02 21:34:08 -0500

    feat :recycle: Refactor CODE Se modularizo la funcion de reemplazar los datos

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import http from 'http';
 import url from 'url';
 import { fileURLToPath } from 'url'
 import path from 'path'
+import { replaceTemplate } from './modules/replaceTemplate.js';
 
 
 const __filename = fileURLToPath(import.meta.url)
@@ -14,19 +15,6 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json` , `utf-8`);
 const dataObject = JSON.parse(data);
 
 //SERVER
-const replaceTemplate = (temp , product) => {
-  let output = temp.replace(/{%ID%}/g, product.id);
-  output = output.replace(/{%NAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  return output;
-};
-
 const server = http.createServer((req , res) => {
   const {query , pathname} = url.parse(req.url , true );
   //overview Page
